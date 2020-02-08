1549b0e5c83cb5e1f96dafbd2e523c7694ab7486
Lytecyde <mik.seljamaa.nagaoka@gmail.com>
2020-02-08 05:11:04 +0200

    refactored test spec

--- spec/abcdef.spec.js
+++ spec/abcdef.spec.js
@@ -7,8 +7,6 @@ describe ("The game is started", function () {
 
         fs.readdir( "./assets", (error, files) => { 
             assetCount = files.length; 
-            console.log(assetCount); 
-            expect(assetCount).toEqual(11);
          });
     });
 
@@ -16,7 +14,7 @@ describe ("The game is started", function () {
         console.log("start message seen");
     });
 
-    // it ("should load all in assets folder", function () {
-    //     expect(assetCount).toEqual(11);
-    // });
+    it ("should load all in assets folder", function () {
+        expect(assetCount).toEqual(11);
+    });
 });
